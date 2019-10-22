0cf026bf5d77f12edaa56cef8c795c1d1787c625
Excel Espina <eespina002@citymail.cuny.edu>
2019-10-22 11:32:18 -0400

    Added detect module and updated pre install script to install jetson inference packages

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ After cloning this repo on the TX2, you must run `./pre-install-script.sh` in or
 #### Python: (TX2 Optimized)
 1. Tensorflow 
 1. OpenCV
+
+### Issues:
+1. `ld -lGL` not found.
+    1. https://devtalk.nvidia.com/default/topic/1051923/jetson-tx2/make-error-usr-bin-ld-cannot-find-lgl-/post/5339745/#5339745
+        1. You may have to specify the full path on libGL.so
+            1. `sudo ln -sfn libGL.so.1.0.0 /usr/lib/aarch64-linux-gnu/libGL.so`
--- pre-install-script.sh
+++ pre-install-script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 TEMP_FOLDER='pre_install'
 PYTHON_VER_CHECK=$(python3 -c '
 import sys;
@@ -11,6 +16,9 @@ else:
 HAS_VENV=$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("virtualenv") else 0)')
 HAS_TENSORFLOW=$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("tensorflow") else 0)')
 HAS_OPENCV=$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("cv2") else 0)')
+HAS_JETSON_INFERENCE=$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("jetson") else 0)')
+
+echo
 
 if [[ $PYTHON_VER_CHECK -eq "1" ]]; then
     echo -e "Python check passed!"
@@ -19,7 +27,24 @@ else
     exit
 fi
 
-if [[ $HAS_VENV -eq "1" ]]; then
+if [[ $HAS_JETSON_INFERENCE -eq "0" ]]; then
+    echo -e "Installing Jetson Inference package..."
+    sleep 3
+    apt-get update
+    apt-get install git cmake libpython3-dev python3-numpy
+    git clone --recursive https://github.com/dusty-nv/jetson-inference
+    cd jetson-inference
+    mkdir build
+    cmake ../
+    make -j4
+    sudo make install
+    sudo ldconfig
+    echo -e "\n\n\n\nJetson Inference package install done!\n"
+else
+    echo -e "Jetson Inference package installed! OK"
+fi
+
+if [[ $HAS_VENV -eq "0" ]]; then
     echo -e "\n\nVirtualenv install done"
     python3 -m pip install virtualenv --user
     echo -e "\n\nVirtualenv install done"
@@ -27,7 +52,7 @@ else
     echo -e "Virtualenv Installed!"
 fi
 
-if [[ $HAS_TENSORFLOW -eq "1" ]]; then
+if [[ $HAS_TENSORFLOW -eq "0" ]]; then
     echo -e "Tensorflow not found. Installing"
     apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
     apt-get install python3-pip
@@ -38,10 +63,18 @@ else
     echo -e "Tensorflow Installed!"
 fi
 
-if [[ $HAS_OPENCV -eq "1" ]]; then
+if [[ $HAS_OPENCV -eq "0" ]]; then
     git clone https://github.com/jetsonhacks/buildOpenCVTX2.git pre_install/Opencv
     ./pre_install/Opencv/buildOpenCV.sh > /dev/null
     echo -e "Opencv Installed."
 else
     echo -e "Opencv Installed!"
 fi
+
+echo -e "\n\n\nPre install check done!"
+sleep 3
+echo -e "Cleaning up!"
+
+rm -rf pre_install/ jetson-inference/
+
+echo -e "Done\n"
